9895b15d03992246eb34e221706ec3a401be828f
Mataye W <wmataye@gmail.com>
2026-09-05 20:23:52 +0200

    imports added to RecipeCard file

--- react-recipe-app-MatayeW/src/components/Recipe/RecipeCard.jsx
+++ react-recipe-app-MatayeW/src/components/Recipe/RecipeCard.jsx
@@ -1,1 +1,7 @@
-import 
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import Card from "../UI/Card";
+import Button from "../UI/Button";
+import { formatCookTime, getTotalTime, getDifficultyEmoji } from "../../utils/helpers";
+import styles from "./Recipe.module.css";
+
