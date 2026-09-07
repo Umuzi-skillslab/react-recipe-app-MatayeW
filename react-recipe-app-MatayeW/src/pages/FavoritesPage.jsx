5e38100b0bcec69eff968c51363f2f23dacb09dc
Mataye W <wmataye@gmail.com>
2026-09-07 15:12:56 +0200

    FavoritesPage.jsx file updated

--- react-recipe-app-MatayeW/src/pages/FavoritesPage.jsx
+++ react-recipe-app-MatayeW/src/pages/FavoritesPage.jsx
@@ -2,3 +2,21 @@ import PropTypes from "prop-types";
 import RecipeList from "../components/Recipe/RecipeList";
 import styles from "./Pages.module.css";
 
+// FavoritesPage reuses RecipeList
+const FavoritesPage = ({ favorites, onFavoriteToggle }) => {
+    return (
+        <div className={styles.page}>
+            <h1>My Favorites</h1>
+            {favorites.length > 0 && <p>{favorites.length} saved recipe(s)</p>}
+
+            <RecipeList recipes={favorites} favorites={favorites} onFavoriteToggle={onFavoriteToggle} />
+        </div>
+    );
+};
+
+FavoritesPage.propTypes = {
+    favorites: PropTypes.array.isRequired,
+    onFavoriteToggle: PropTypes.func.isRequired,
+};
+
+export default FavoritesPage;
