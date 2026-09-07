a8a9ff4f9a487761e2c8250b69222382c0e101e9
Mataye W <wmataye@gmail.com>
2026-09-07 11:19:43 +0200

    Comment added to MealPlanner.module.css file and imports added into App.jsx file

--- react-recipe-app-MatayeW/src/App.jsx
+++ react-recipe-app-MatayeW/src/App.jsx
@@ -1,6 +1,13 @@
-
+import { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navigation/Navbar';
+import Home from "./pages/Home";
+import RecipesPages from "./pages/RecipesPage";
+import RecipeDetail from "./components/Recipe/RecipeDetail";
+import MealPlannerPage from "./pages/MealPlannerPage";
+import FavoritesPage from "./pages/FavoritesPage";
+import NotFound from "./pages/NotFound";
+import { recipesData } from "./data/recipesData";
 
 function App() {
   return (
